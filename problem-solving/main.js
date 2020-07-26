6ca3f164f52762db1425e0b18a8b47e978230ee5
jane muthoni <muchirijane@outlook.com>
2020-07-26 20:48:02 +0300

    solution 2

--- problem-solving/main.js
+++ problem-solving/main.js
@@ -5,28 +5,49 @@ const results = addup(2,6);
 console.log(results);
 
 // question 
-//write a function that takes a string and converts it into separate individual characters
+//write a function that takes a alphanumeric string and converts it into separate individual characters
 // output: can take any numbers or any text whether uppercase or lowercase
-function countStr (str){
-    let result = {};
 
-    for(let i = 0; i < str.length; i++) {
-        let char = str[i].toLowerCase();
-        const regex = /[\w+]/;
-        if(regex.test(char)) {
-            if(result[char] > 0){
-                result[char]++;
-            }else{
-                result[char] = 1;
-            }
-        }
-        
-    }
+//solution one
+// function countStr (str){
+//     let result = {};
+
+//     for(let i = 0; i < str.length; i++) {
+//         let char = str[i].toLowerCase();
+//         const regex = /[\w+]/;
+//         if(regex.test(char)) {
+//             if(result[char] > 0){
+//                 result[char]++;
+//             }else{
+//                 result[char] = 1;
+//             }
+//         }
+//     }
     
-    return result;
+//     return result;
+// }
+
+// let string = countStr('Hello heeeeyyy gooooood');
+// console.log(string);
+
+//solution two 
+let str = {};
+const countStr = (str) =>{
+    let result = [...str];
+    const regex = /[\w+]/;
+    if(regex.test(result)) {
+        return result;
+    } 
 }
 
-let string = countStr('Hello heeeeyyy gooooood');
+let string = countStr('Hello heeeeyyy 56789 gooooood');
 console.log(string);
 
+const double = (... nums) =>{
+    console.log(nums);
+   return nums.map(num => num * 2);
+}
+console.log(double(1,2,3,4,5,6,7,8,9,9));
 
+const people = ["Mercury", "John", "James", "George"];
+console.log(...people);
---
+++ problem-solving/note.md
@@ -0,0 +1,5 @@
+## Steps to take to solve any problem
+#### Understand the problem
+#### Explore concrete examples
+#### Break it down
+#### Solve or simplify
